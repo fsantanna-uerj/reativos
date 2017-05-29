4bfa46a8aa00669a1e13ba6f3958a0e58e232e83
fsantanna <francisco.santanna@gmail.com>
2017-05-29 07:15:33 -0300

    ...

--- code/sdl/00_hello.c
+++ code/sdl/00_hello.c
@@ -10,7 +10,7 @@ int main (int argc, char* args[])
 
     SDL_Window* window = SDL_CreateWindow("Hello World!",
                             SDL_WINDOWPOS_UNDEFINED,
-                            SDL_WINDOW POS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED,
                             640, 480, SDL_WINDOW_SHOWN);
     assert(window != NULL);
 
